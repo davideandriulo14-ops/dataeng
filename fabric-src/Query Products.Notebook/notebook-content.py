3c1523592c218d6701c456be8331d3d28b878057
davideandriulo14-ops <davide.andriulo14@gmail.com>
2026-09-17 17:40:28 +0200

    Committing 2 items from workspace 1889f1eb-4840-4e4e-9f3a-1881cf34d4bd

---
+++ fabric-src/Query Products.Notebook/notebook-content.py
@@ -0,0 +1,37 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "691972c4-ccd1-4c7f-b837-c7ddc004c033",
+# META       "default_lakehouse_name": "lh_edu",
+# META       "default_lakehouse_workspace_id": "1889f1eb-4840-4e4e-9f3a-1881cf34d4bd",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "691972c4-ccd1-4c7f-b837-c7ddc004c033"
+# META         }
+# META       ]
+# META     },
+# META     "warehouse": {
+# META       "known_warehouses": []
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
